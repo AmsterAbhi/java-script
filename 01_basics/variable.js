adf37a6f697a6830ae6b63b3dae4dfd677d26d38
AmsterAbhi <121009417+AmsterAbhi@users.noreply.github.com>
2024-07-13 19:17:46 +0530

    Add files via upload

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 112446855
+let accountEmail = "abhishek@gmail.com"
+var acccountPassword = "12345"
+accountcity = "delhi"
+
+// accountId = 2 // changing variable with constant datatype is not allowed
+
+let accountState;
+
+accountEmail = "abhi@gmail.com"
+acccountPassword= "0987"
+accountcity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,acccountPassword,accountcity,accountState])

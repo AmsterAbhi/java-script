7aa186e8f79148519a38634e0294cfc37bea9dd6
AmsterAbhi <121009417+AmsterAbhi@users.noreply.github.com>
2024-07-13 14:39:22 +0000

    commit

---
+++ 01_basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3)  // it is allowed is brower not in nodejs not using this syntax
+
+let name = "Abhishek"
+let age = 21
+let isLoggedIn = true
+
+
+// string => ""
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// null => standalone value
+// undefined => value is not declared yet
+// symbol => unique
+// object
+
+console.log(typeof "Abhishek");
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof null);            // null is a object datatype
+console.log(typeof undefined);       // undefined is datatype undefined
